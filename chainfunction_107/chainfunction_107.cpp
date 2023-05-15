3efa34581be57f1f398c2a1cc7f8184eaee9442d
rezaikram <muhammadikramriza@gmail.com>
2023-05-15 19:03:43 +0700

    string judul

---
+++ chainfunction_107/chainfunction_107.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class buku
+{
+	string judul;
+
+public:
+	buku setjudul(string judul)
+	{
+		this->judul = judul;
+		return *this; //chain function
+	}
+	string getjudul()
+	{
+		return this->judul;
+	
+	}
+} bukunya;
+
+int main()
+{
+	// bukunya.setbuku("MATEMATIKA");
+	//cout << bukunya.getjudul();
+	cout << bukunya.setjudul("MATEMATIKA").getjudul(); //chain function calls
+	return 0;
+}
