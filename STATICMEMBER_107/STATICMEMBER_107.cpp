45916a93e12342f7d073017a0b78393b281eb7b8
rezaikram <muhammadikramriza@gmail.com>
2023-05-15 18:47:14 +0700

    int main

--- STATICMEMBER_107/STATICMEMBER_107.cpp
+++ STATICMEMBER_107/STATICMEMBER_107.cpp
@@ -25,6 +25,18 @@ void mahasiswa::printALL() {
 	cout << endl;
 }
 
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printALL();
+	mhs2.printALL();
+	mhs3.printALL();
+	mhs4.printALL();
+	return 0;
+}
 
 
 	
