ffa450e5f2d0b869a699e76e0f108591efea755e
rezaikram <muhammadikramriza@gmail.com>
2023-05-15 19:18:12 +0700

    definisi member function

--- destruktor_107/destruktor_107.cpp
+++ destruktor_107/destruktor_107.cpp
@@ -18,30 +18,3 @@ angka::angka(int i) {
 	isiData();
 }
 
-angka::~angka() {
-	cout << endl;
-	cetakData();
-	delete[]arr;
-	cout << "Alamat Array Sudah dilepaskan" << endl;
-}
-
-void angka::cetakData() {
-	for (int i = 1; i <= panjang; i++) {
-		cout << i << " =" <<arr [i] << endl;
-	}
-}
-
-void angka::isiData() {
-	for (int i = 1; i <= panjang; i++) {
-		cout << i << ""; cin >> arr[i];
-	}
-	cout << endl;
-}
-
-int main() {
-	angka belajarcpp(3);
-	angka* ptrBelajarcpp = new angka(5);
-	delete ptrBelajarcpp;
-
-	return 0;
-}
